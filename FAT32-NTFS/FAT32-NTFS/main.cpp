6aa7f438ea76ff35f690dfea0b33e71f8bf11185
tutam1232 <135777473+tutam1232@users.noreply.github.com>
2023-10-15 21:52:26 +0700

    Create project

---
+++ FAT32-NTFS/FAT32-NTFS/main.cpp
@@ -0,0 +1,42 @@
+﻿#include "function.h"
+
+
+int main(int argc, char** argv) //file main tui test thoi, mn cứ xóa nhe
+{
+
+    BYTE sector[512];
+    const char* outputFileName = "sector_data1.bin";
+
+    //342258352128: from PhysicalDrive0
+    //3221225472 =  786 432 * 8 * 512: from F
+    ReadSector(L"\\\\.\\F:", 3221225472, sector);   
+    
+    
+    /*FILE* outputFile = fopen(outputFileName, "wb");
+
+    if (outputFile == NULL)
+    {
+        printf("Error opening the output file.\n");
+        return 1;
+    }
+    int bytesWritten = fwrite(sector, 1, 512, outputFile);
+
+    if (bytesWritten != 512)
+    {
+        printf("Error writing to the output file.\n");
+        fclose(outputFile);
+        return 1;
+    }
+
+    printf("Sector data successfully written to %s\n", outputFileName);
+
+    fclose(outputFile);*/
+
+    BYTE test[2];
+    test[0] = sector[21];
+    test[1] = sector[20];
+
+    cout << test;
+
+    return 0;
+}
