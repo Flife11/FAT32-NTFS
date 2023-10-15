fa75ef30fb8c0a690d11df784db2d4b96ed30ca1
tutam1232 <135777473+tutam1232@users.noreply.github.com>
2023-10-15 22:07:30 +0700

    Create project

---
+++ FAT32-NTFS/function.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+/// <summary>
+/// Hàm để đọc 1 sector
+/// </summary>
+/// <param name="drive">: tên ổ đĩa</param>
+/// <param name="readPoint">: bắt đầu đọc từ byte thứ readPoint của ổ drive</param>
+/// <param name="sector">: biến lưu nội dung sector</param>
+/// <returns></returns>
+int ReadSector(LPCWSTR  drive, unsigned long long readPoint, BYTE sector[512]);
